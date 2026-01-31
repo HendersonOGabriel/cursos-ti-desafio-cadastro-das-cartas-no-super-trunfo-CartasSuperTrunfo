673dbcca917c98286138a48640b5a174dd4024b9
Gabriel Henderson <133170280+HendersonOGabriel@users.noreply.github.com>
2026-01-31 21:34:05 +0000

    Concluído o código de adição de duas cartas Super Trunfo ao jogo.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,71 @@
 //Teste larissa
 
 int main() {
+
+
+    // Declaração das variáveis para os atributos das cidades
+
+    int populacao, pontosturisticos, populacao2, pontosturisticos2; 
+    float area, pib, area2, pib2;
+    char estado[50], cidade[50], estado2[50], cidade2[50], codigo[10], codigo2[10];
+
+
+    // requerimentos do usuário para 1ª carta:
+
+    printf("Digite o nome do 1º estado: ");
+    scanf("%s", estado);
+
+    printf("Digite o nome da 1ª cidade: ");
+    scanf("%s", cidade);
+
+    printf("Digite o código da cidade: ");
+    scanf("%s", codigo);
+
+    printf("Digite o número de habitantes: ");
+    scanf("%d", &populacao);
+
+    printf("Digite o número de pontos turísticos: ");
+    scanf("%d", &pontosturisticos);
+
+    printf("Digite a área da cidade: ");
+    scanf("%f", &area);
+
+    printf("Digite o PIB da cidade: ");
+    scanf("%f", &pib);
+
+
+    // requerimentos do usuário para 2ª carta:
+
+    printf("\nDigite o nome do 2º estado: ");
+    scanf("%s", estado2);
+
+    printf("Digite o nome da 2ª cidade: ");
+    scanf("%s", cidade2);
+
+    printf("Digite o código da cidade: ");
+    scanf("%s", codigo2);
+
+    printf("Digite o número de habitantes: ");
+    scanf("%d", &populacao2);
+
+    printf("Digite o número de pontos turísticos: ");
+    scanf("%d", &pontosturisticos2);
+
+    printf("Digite a área da cidade: ");
+    scanf("%f", &area2);
+
+    printf("Digite o PIB da cidade:");
+    scanf("%f", &pib2);
+
+    // exibição 1ª e 2ª cartas:
+
+    printf("\n\n1ª Carta de Cidade:\nEstado: %s\nCidade: %s\nPopulação: %d\nPontos Turísticos: %d\nÁrea da cidade: %.2fm²\nPIB da cidade: R$%.0f\n\n", estado, cidade, populacao, pontosturisticos, area, pib);
+
+    
+    printf("2ª Carta de Cidade:\nEstado: %s\nCidade: %s\nPopulação: %d\nPontos Turísticos: %d\nÁrea da cidade: %.2fm²\nPIB da cidade: R$%.0f\n\n", estado2, cidade2, populacao2, pontosturisticos2, area2, pib2);
+
+
+
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
