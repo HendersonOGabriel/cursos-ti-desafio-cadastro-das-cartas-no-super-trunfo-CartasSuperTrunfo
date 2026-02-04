ac2fc0f2116a2f4068cbf14d0287d17c11b46838
Gabriel Henderson <133170280+HendersonOGabriel@users.noreply.github.com>
2026-02-04 23:16:52 +0000

    Adicionada funcionalidade de cálculo de densidade populacional e pib per capta ao sistema.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -11,8 +11,10 @@ int main() {
 
     // Declaração das variáveis para os atributos das cidades
 
-    int populacao, pontosturisticos, populacao2, pontosturisticos2; 
-    float area, pib, area2, pib2;
+    int populacao, pontosturisticos, populacao2, pontosturisticos2;
+    long int pib, pib2;
+    // mudei o pib, pois o valor do pib é muito grande, e o tipo float não é adequado para armazenar valores tão grandes, pois pode ocorrer perda de precisão. O tipo long int é mais apropriado para armazenar valores inteiros grandes, como o PIB de uma cidade.
+    float area, densidade, pibpercapta, area2, densidade2, pibpercapta2;
     char estado[50], cidade[50], estado2[50], cidade2[50], codigo[10], codigo2[10];
 
 
@@ -37,7 +39,7 @@ int main() {
     scanf("%f", &area);
 
     printf("Digite o PIB da cidade: ");
-    scanf("%f", &pib);
+    scanf("%ld", &pib);
 
 
     // requerimentos do usuário para 2ª carta:
@@ -61,15 +63,44 @@ int main() {
     scanf("%f", &area2);
 
     printf("Digite o PIB da cidade:");
-    scanf("%f", &pib2);
+    scanf("%ld", &pib2);
 
-    // exibição 1ª e 2ª cartas:
 
-    printf("\n\n1ª Carta de Cidade:\nEstado: %s\nCidade: %s\nPopulação: %d\nPontos Turísticos: %d\nÁrea da cidade: %.2fm²\nPIB da cidade: R$%.0f\n\n", estado, cidade, populacao, pontosturisticos, area, pib);
+    // Cálculo de densidade populacional e PIB per capita:
 
+    // Carta 1:
+    densidade = populacao / area;
+    pibpercapta = pib / populacao;
+
+    // Carta 2:
+    densidade2 = populacao2 / area2;
+    pibpercapta2 = pib2 / populacao2;
+
+
+    // exibição 1ª carta:
+
+    printf("\n--- Carta 1 ---\n");
+    printf("Estado: %s\n", estado);
+    printf("Cidade: %s\n", cidade);
+    printf("Código: %s\n", codigo);
+    printf("População: %d\n", populacao);
+    printf("Pontos Turísticos: %d\n", pontosturisticos);
+    printf("Área: %.2f km²\n", area);
+    printf("PIB: R$ %ld\n", pib);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidade);
+    printf("PIB per Capita: R$ %.2f\n", pibpercapta);
     
-    printf("2ª Carta de Cidade:\nEstado: %s\nCidade: %s\nPopulação: %d\nPontos Turísticos: %d\nÁrea da cidade: %.2fm²\nPIB da cidade: R$%.0f\n\n", estado2, cidade2, populacao2, pontosturisticos2, area2, pib2);
-
+    // exibição 2ª carta:
+    printf("\n--- Carta 2 ---\n");
+    printf("Estado: %s\n", estado2);
+    printf("Cidade: %s\n", cidade2);
+    printf("Código: %s\n", codigo2);
+    printf("População: %d\n", populacao2);
+    printf("Pontos Turísticos: %d\n", pontosturisticos2);
+    printf("Área: %.2f km²\n", area2);
+    printf("PIB: R$ %ld\n", pib2);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidade2);
+    printf("PIB per Capita: R$ %.2f\n", pibpercapta2);
 
 
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
